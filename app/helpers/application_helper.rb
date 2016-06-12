16b1f82b6269858a3a9f0ed09e2809da1f17d658
acandael <info@anthonycandaele.com>
2016-06-12 09:48:56 +0200

    refactor has_profile? helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def has_profile?
-    current_member.profile != nil && current_member.profile.id != nil
+    current_member.profile != nil && !current_member.profile.new_record?
   end
 end
