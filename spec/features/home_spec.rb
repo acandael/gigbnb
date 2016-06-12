afe5e362db1a549e488ef4a4c81d74f248b4e059
acandael <info@anthonycandaele.com>
2016-06-12 14:42:39 +0200

    implement header

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature "visiting the homepage" do
   scenario "the visitor sees welcome text" do
     visit root_path
-    expect(page).to have_content("Welcome")
+    expect(page).to have_content("Find a gig and a sleeping place")
   end
 end
