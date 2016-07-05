0bad3cf1b8e355ff279dc9d6a77e3c0c15833fdd
acandael <info@anthonycandaele.com>
2016-07-05 16:32:19 +0200

    validate numericality for numeric field location form

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,10 @@ class Location < ActiveRecord::Base
   belongs_to :member
 
   validates :title, presence: true
+  validates :postal_code, numericality: true
+  validates :beds, numericality: true
+  validates :guests, numericality: true
+  validates :price, numericality: true
 
   def country_name(country_code)
     country = ISO3166::Country[country_code]
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Location, type: :model do
   it { should belong_to(:member) }
   it { should validate_presence_of(:title) }
+  it { should validate_numericality_of(:postal_code) }
+  it { should validate_numericality_of(:beds) }
+  it { should validate_numericality_of(:guests) }
+  it { should validate_numericality_of(:price) }
 end
