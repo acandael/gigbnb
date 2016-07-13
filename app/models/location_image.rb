5eb747ac537dc58b5a85a79b69bda83c386a1831
acandael <info@anthonycandaele.com>
2016-07-13 10:34:27 +0200

    pluralize number of beds and guests

--- app/models/location_image.rb
+++ app/models/location_image.rb
@@ -1,7 +1,7 @@
 class LocationImage < ActiveRecord::Base
 
   belongs_to :location
-  has_attached_file :picture, styles: { medium: "300x300#" }
+  has_attached_file :picture, styles: { small: "150x150#", medium: "300x300#" }
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 
   after_create :set_picture_order
