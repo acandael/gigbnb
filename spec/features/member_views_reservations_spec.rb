6339a9d602fe123ee8bcd1065d35903cda46aed5
acandael <info@anthonycandaele.com>
2016-08-01 17:45:28 +0200

    implement feature spec member views his reservations

--- spec/features/member_views_reservations_spec.rb
+++ spec/features/member_views_reservations_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 feature "member views his reservations" do
   let(:member) { FactoryGirl.create(:member) }
-  let(:location) { FactoryGirl.create(:location, member_id: member.id) }
-  let(:reservation) { FactoryGirl.create(:reservation, member_id: member.id, location_id: location.id) }
 
   before do
     login_as(member, :scope => :member)
   end
 
-  it "views the reservation", js: true do
-   visit member_reservations_path(member)
-   save_and_open_page
-   expect(page).to have_content "Reservation 1" 
+    scenario "views the reservation", js: true do
+        location = FactoryGirl.create(:location, member_id: member.id) 
+    reservation = FactoryGirl.build(:reservation, member_id: member.id, location_id: location.id) 
+        reservation.save(validate: false)
+        
+    visit member_reservations_path(member)
+
+    expect(page).to have_content "Reservation 1" 
   end
 end
