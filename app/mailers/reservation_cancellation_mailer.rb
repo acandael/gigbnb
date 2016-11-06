4345de87f6d5aed83a5a597cd5d87d67d875bffa
acandael <info@anthonycandaele.com>
2016-11-06 17:50:51 +0100

    edit mailer views for reservation cancellation

--- app/mailers/reservation_cancellation_mailer.rb
+++ app/mailers/reservation_cancellation_mailer.rb
@@ -3,6 +3,7 @@ class ReservationCancellationMailer < ApplicationMailer
   def send_cancellation_to_guest(reservation)
     @greeting = "You have cancelled a reservation"
     @guest = reservation.member
+    @location = reservation.location
 
     mail(to: @guest.email, subject: "reservation cancellation")
   end
@@ -12,6 +13,9 @@ class ReservationCancellationMailer < ApplicationMailer
     @location = reservation.location
     @host = @location.member
     @guest = reservation.member
+    profile = @guest.profile
+    @first_name = profile.first_name
+    @last_name = profile.last_name
 
     mail(to: @host.email, from: @guest.email, subject: "reservation cancellation")
   end
--- spec/mailers/previews/reservation_cancellation_mailer_preview.rb
+++ spec/mailers/previews/reservation_cancellation_mailer_preview.rb
@@ -3,12 +3,14 @@ class ReservationCancellationMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/reservation_cancellation_mailer/send_cancellation_to_guest
   def send_cancellation_to_guest
-    ReservationCancellationMailer.send_cancellation_to_guest
+    reservation = Reservation.last
+    ReservationCancellationMailer.send_cancellation_to_guest(reservation)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/reservation_cancellation_mailer/send_cancellation_to_host
   def send_cancellation_to_host
-    ReservationCancellationMailer.send_cancellation_to_host
+    reservation = Reservation.last
+    ReservationCancellationMailer.send_cancellation_to_host(reservation)
   end
 
 end
