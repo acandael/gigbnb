0fe252289e9535e9bfb86644e905bcdd9a4cc139
acandael <info@anthonycandaele.com>
2016-11-19 14:25:33 +0100

    edit reservation confirmation mail messages

--- app/mailers/reservation_confirmation_mailer.rb
+++ app/mailers/reservation_confirmation_mailer.rb
@@ -3,11 +3,13 @@ class ReservationConfirmationMailer < ApplicationMailer
   def send_customer_reservation_confirmation(reservation)
     @body = "You have reserved the location below."
     @location = reservation.location
+    @address = @location.address
     @host = @location.member
     @guest = reservation.member
     profile = @host.profile
     @first_name = profile.first_name
     @last_name = profile.last_name
+    @reservation = reservation
     mail(to: @guest.email, subject: "reservation confirmation")
   end
 
@@ -20,6 +22,7 @@ class ReservationConfirmationMailer < ApplicationMailer
     @address = @location.address
     @first_name = profile.first_name
     @last_name = profile.last_name
+    @reservation = reservation
 
     mail(to: @host.email, from: @guest.email, subject: "A customer reserved a location")
   end
